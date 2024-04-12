78817882c7d5bcc39e842f46e37e3a6f1379ca2a
atijuF <162565598+atijuF@users.noreply.github.com>
2024-04-12 08:56:19 +0000

    [Add]ログイン機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   
+  devise_for :users
   root to:"homes#top"
+  get 'homes/top'   => "homes#top"
+  get 'homes/about' => "homes#about"
+  get '/about' => "homes#about"
+  
   resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
